eac11644d6d08c1662d62668a52caa288f6d12f2
Manimaran <manimaran10052000@gmail.com>
2025-06-18 16:02:45 +0530

    API changed

--- src/global.js
+++ src/global.js
@@ -1,5 +1,5 @@
 // export const API = "https://666f1937f1e1da2be521f8a9.mockapi.io";  //->Mock API 
 
-export const API ="http://localhost:4000";  //->Local Mongo compass API
+// export const API ="http://localhost:4000";  //->Local Mongo compass API
 
-// export const API = "https://node-crud-eta.vercel.app/" //-> Vercel API
+export const API = "https://node-crud-eta.vercel.app/" //-> Vercel API
